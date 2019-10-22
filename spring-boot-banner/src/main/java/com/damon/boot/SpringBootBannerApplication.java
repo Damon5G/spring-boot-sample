e57c4a864bb20881f32df9b863374d1d9fd83550
ali_wzc <944375586@qq.com>
2019-10-22 15:33:16 +0800

    关闭banner

--- spring-boot-banner/src/main/java/com/damon/boot/SpringBootBannerApplication.java
+++ spring-boot-banner/src/main/java/com/damon/boot/SpringBootBannerApplication.java
@@ -1,5 +1,6 @@
 package com.damon.boot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootBannerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootBannerApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(SpringBootBannerApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
+        //SpringApplication.run(SpringBootBannerApplication.class, args);
     }
 
 }
