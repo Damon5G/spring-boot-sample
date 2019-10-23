d3ab166d1e5c2b3a3208648711d39f1d52b84c19
ali_wzc <944375586@qq.com>
2019-10-23 15:02:39 +0800

    redis 分布式锁

---
+++ spring-boot-lock-redis/src/main/java/com/damon/lock/config/RedisLockConfiguration.java
@@ -0,0 +1,17 @@
+package com.damon.lock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.integration.redis.util.RedisLockRegistry;
+
+/**
+ * @author damon
+ */
+@Configuration
+public class RedisLockConfiguration {
+    @Bean
+    public RedisLockRegistry redisLockRegistry(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisLockRegistry(redisConnectionFactory, "sprig-cloud", 5000L);
+    }
+}
